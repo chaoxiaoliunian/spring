f6df41df00c4098fc9124778fe9665317953094b
sjqi <1013988871@qq.com>
2019-04-12 10:44:57 +0800

    Controller 增强Demo完成

---
+++ ssm-demo/src/main/java/com/sjqi/ssm/advice/MyControllerAdvice.java
@@ -0,0 +1,47 @@
+package com.sjqi.ssm.advice;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName MyControllerAdvice
+ * @Description 对所有的Controller 进行增强
+ * @Author sjqi
+ * @Date 10:20 2019/4/12
+ * @Version 1.0
+ **/
+@ControllerAdvice
+public class MyControllerAdvice {
+    @ModelAttribute
+    public Map<String, Object> putModelMap() {
+        //应用到所有的@RequestMapping 在其执行前，把所有的返回值放入ModelMap中
+        Map<String, Object> map = new HashMap<>();
+        map.put("test", "test");
+        System.out.println("Controller 增强：将参数放入ModelMap");
+        return map;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        //应用到@RequestMapping，在其执行前，进行参数绑定
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+        System.out.println("initBinder");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public String handleException(Exception e) {
+        //应用到所有的@RequestMapping，在其抛出异常时指定。
+        System.out.println("Controller 增强：异常处理");
+        return e.getMessage();
+    }
+}
--- ssm-demo/src/main/java/com/sjqi/ssm/controller/UserController.java
+++ ssm-demo/src/main/java/com/sjqi/ssm/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.sjqi.ssm.controller;
 
-import com.sjqi.ssm.model.ParamsModel;
 import com.sjqi.ssm.model.ParamsModel2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
