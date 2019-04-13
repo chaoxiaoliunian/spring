11a1d3b8fda57e0aae6430ba95a2c7c89b053e9f
sjqi <1013988871@qq.com>
2019-04-13 18:37:53 +0800

    配置单一数据源

---
+++ demo/src/main/java/com/sjqi/springboot/DemoApplicaton.java
@@ -0,0 +1,53 @@
+package com.sjqi.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @ClassName SpringBootApplicaton
+ * @Description SpringBoot 的入口
+ * @Author sjqi
+ * @Date 17:09 2019/4/13
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@Slf4j
+//lombok注解，需要idea 安装对应插件
+public class DemoApplicaton implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicaton.class);
+    }
+
+
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info(conn.toString());
+        conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO")
+                .forEach(row -> log.info(row.toString()));
+    }
+}
